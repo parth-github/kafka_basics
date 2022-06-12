c48b8e98c61419c629ef9ff18bb0a4c54189bc3c
Smriti <linuxriti@gmail.com>
2022-06-12 21:53:10 +0530

    initial commit

---
+++ src/main/java/org/example/demos/kafka/ProducerDemo.java
@@ -0,0 +1,22 @@
+package org.example.demos.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class ProducerDemo {
+
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+
+        // create producer properties
+
+        // create the producer
+
+        // send data
+
+        // flush and close the producer
+    }
+}
